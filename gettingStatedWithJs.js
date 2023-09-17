4f2a58046e1bad48c1b86cb94ba62c7034c6086b
Bahurudeen-a <bahurudeenabbas28@gmail.com>
2023-09-17 11:05:58 +0530

    gettingStatedWithJs.js

---
+++ gettingStatedWithJs.js
@@ -0,0 +1,15 @@
+console.log("Hello Javascript");
+console.warn("how are you");
+console.error("ERORRRRRRRRR");
+
+//A) Variable
+var bag = 'Books';
+var fullName = 'Bahurudeen A'
+bag = 'tiffin box' // This is executable not books
+var qwerty; //declaration
+var count = 20; //initialization
+
+console.log(count);
+console.log(qwerty);
+console.log(bag);
+console.log(fullName);
