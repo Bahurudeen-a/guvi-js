b14beebea780fb97dcf1dbb06620e6d69c4eb761
Bahurudeen-a <bahurudeenabbas28@gmail.com>
2023-09-18 11:49:46 +0530

    TypeCasting

---
+++ typeCast.js
@@ -0,0 +1,7 @@
+let contact = "1343101823";
+let convertContact = +contact; //if we remove + it is a string
+console.log(typeof convertContact);
+
+let isLoggedIn = false;
+let convertIsLoggedIn = isLoggedIn + "";
+console.log(typeof convertIsLoggedIn);
