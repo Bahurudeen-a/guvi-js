421d9e8f10f78a26b991a33a9d0cbe617006efac
Bahurudeen-a <bahurudeenabbas28@gmail.com>
2023-09-18 11:17:21 +0530

    Arithmetic Operations

---
+++ arithmeticOperation.js
@@ -0,0 +1,11 @@
+let english = 60;
+let webDevelopment = 100;
+let dataStructure = 98;
+let oops = 28;
+let git = 74;
+
+console.log(english + webDevelopment + dataStructure + oops + git);
+console.log(dataStructure - webDevelopment);
+console.log(dataStructure % oops);
+console.log(oops * english);
+console.log(git / dataStructure);
