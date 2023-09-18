d3a404f36bde31d2889fd2c458d26a6b424cd053
Bahurudeen-a <bahurudeenabbas28@gmail.com>
2023-09-18 10:42:08 +0530

    numbersInJs.js

---
+++ numbersInJs.js
@@ -0,0 +1,36 @@
+console.log("Numbers in JavaScript");
+
+var age = 24;
+var salary = 25000;
+var bodyWeight = 56.76;
+var contact = 1234532341;
+var randomNumber = 10000.0009;
+var negativeNum = -823;
+
+console.log(age);
+console.log(salary);
+console.log(bodyWeight);
+console.log(contact);
+console.log(randomNumber);
+console.log(negativeNum);
+
+console.log(typeof randomNumber);
+console.log(typeof negativeNum);
+
+let x=120;
+console.log(x);
+console.log(x.toString())
+console.log((120+80).toString());
+
+var y =  -9.523434634;
+console.log(y);
+console.log(y.toExponential(2));
+console.log(y.toFixed());
+console.log(y.toPrecision(4));
+console.log((20*18).valueOf());
+
+var age1 = "44";
+console.log(Number(age1));
+console.log(new Date);
+console.log(parseInt("8727358"));
+console.log(parseFloat("6542265.2876436"));
