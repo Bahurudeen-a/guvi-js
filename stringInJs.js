ddd530d0a6b3720a9154a9e7cb27dae248dd9e7e
Bahurudeen-a <bahurudeenabbas28@gmail.com>
2023-09-23 12:31:47 +0530

    string in js

---
+++ stringInJs.js
@@ -0,0 +1,10 @@
+let firstName = "Bahurudeen";
+let lastName = "Abbas";
+console.log(firstName.length, lastName.length);
+
+console.log(firstName.slice(6));
+console.log(firstName.slice(1,6));
+console.log(firstName.slice(-6));
+console.log(firstName.slice(-1));
+console.log(firstName.slice(-1, -6));
+console.log(firstName.substring(1,4));
